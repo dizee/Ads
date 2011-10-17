c4f5dbfc9e9a78cf4b5bcb62839e09e2e0bdda28
jp <jp@stoneits.com>
2011-10-17 16:28:46 -0400

    Empty Ads

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ads::Application.config.secret_token = '017f2210d0ac9196981fb93f90bd45f8cee12967afc593d6419dbca6a90b40028888558616fdfebc22ffaf1b77d5f11c12594a47c9dffe0b13e9897507f1409c'
